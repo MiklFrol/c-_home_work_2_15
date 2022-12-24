e658937533af40751d681ed5ec0e9342cf1e9839
MiklFrol <miklfrol@mail.ru>
2022-12-24 23:56:42 +0300

    Задача 15 выполнена

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа принимает цыфру дня недели, и проверяет, является ли этот день выходным
+
+Console.Clear();
+
+Console.WriteLine("Введите номер дня недели:");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number > 5 && number < 8)
+    {
+        Console.WriteLine ($"Да, {number}-ой день недели - выходной!");
+    }
+ else if (number >= 1 && number < 6)
+    {
+        Console.WriteLine ($"Нет, {number} - рабочий день недели!");
+    }
+else if (number > 7)
+{
+    Console.WriteLine ($"В неделе всего 7 дней!");
+}
